1458da34b646c730ef06c1ace3227a86c73c1327
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-07-22 12:51:49 +0400

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Aleksandr Mamlygo on 22.07.23.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
